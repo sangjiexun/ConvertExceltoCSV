314c5a97037790a20fa2a66192fa68d2a3c557eb
your user name <tsuniki.miniki@gmail.com>
2016-12-08 02:16:24 +0900

    created the view transition

---
+++ WEB-INF/src/hoge/ConvertServlet.java
@@ -0,0 +1,27 @@
+package hoge;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ConvertServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/Result.jsp");
+		dispatcher.forward(request, response);
+	}
+	
+	
+	
+}
