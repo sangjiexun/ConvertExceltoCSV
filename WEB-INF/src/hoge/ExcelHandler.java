40b36ac99e408fbfd3191d8cdb216bca5075efc0
your user name <tsuniki.miniki@gmail.com>
2016-12-15 01:02:51 +0900

    bugfix added japanese calendar

--- WEB-INF/src/hoge/ExcelHandler.java
+++ WEB-INF/src/hoge/ExcelHandler.java
@@ -29,38 +29,46 @@ public class ExcelHandler {
 			if (DateUtil.isCellDateFormatted(cell)) {
 				SimpleDateFormat sdf = new SimpleDateFormat("yyyy/M/d");
 				String strDate = sdf.format(cell.getDateCellValue()) + " 22:00";
-				logger.trace("(row.col) = (" + cell.getRowIndex() + "," + cell.getColumnIndex() + ")" + "cellValue = " + cell);
+				logger.trace("Cell.CELL_TYPE_NUMERIC:" + "(row.col) = (" + cell.getRowIndex() + "," + cell.getColumnIndex() + ")" + "cellValue = " + strDate);
 				tempCellValue = DOUBLEQUOT + strDate + DOUBLEQUOT;
 			} else {
-				logger.trace("(row.col) = (" + cell.getRowIndex() + "," + cell.getColumnIndex() + ")" + "cellValue = " + cell);
+				//TODO
+				//HACK
+				if(cell.getNumericCellValue() > 42370){
+					SimpleDateFormat sdf = new SimpleDateFormat("yyyy/M/d");
+					String strDate = sdf.format(cell.getDateCellValue()) + " 22:00";
+					logger.trace("DDDD = " + strDate);
+				}
+				
+				logger.trace("Cell.CELL_TYPE_NUMERIC:" + "(row.col) = (" + cell.getRowIndex() + "," + cell.getColumnIndex() + ")" + "cellValue = " + cell);
 				tempCellValue = DOUBLEQUOT + cell.getNumericCellValue() + DOUBLEQUOT;
 			}
 			break;
 		case Cell.CELL_TYPE_STRING: // 1
-			logger.trace("(row.col) = (" + cell.getRowIndex() + "," + cell.getColumnIndex() + ")" + "cellValue = " + cell);
+			logger.trace("Cell.CELL_TYPE_STRING:" + "(row.col) = (" + cell.getRowIndex() + "," + cell.getColumnIndex() + ")" + "cellValue = " + cell);
 			tempCellValue = DOUBLEQUOT + cell.getStringCellValue() + DOUBLEQUOT;
 			break;
 		case Cell.CELL_TYPE_FORMULA: // 2
 			CreationHelper crateHelper = wb.getCreationHelper();
 			FormulaEvaluator evaluator = crateHelper.createFormulaEvaluator();
 			evaluator.evaluateInCell(cell);
-			logger.trace("(row.col) = (" + cell.getRowIndex() + "," + cell.getColumnIndex() + ")" + "cellValue = " + cell);
+			logger.trace("Cell.CELL_TYPE_FORMULA:" + "(row.col) = (" + cell.getRowIndex() + "," + cell.getColumnIndex() + ")" + "cellValue = " + cell);
 			tempCellValue = DOUBLEQUOT + evaluator.evaluateInCell(cell) + DOUBLEQUOT;
 			break;
 		case Cell.CELL_TYPE_BLANK: // 3
-			logger.trace("(row.col) = (" + cell.getRowIndex() + "," + cell.getColumnIndex() + ")" + "cellValue = " + cell);
+			logger.trace("Cell.CELL_TYPE_BLANK:" + "(row.col) = (" + cell.getRowIndex() + "," + cell.getColumnIndex() + ")" + "cellValue = " + cell);
 			tempCellValue = DOUBLEQUOT + "" + DOUBLEQUOT;
 			break;
 		case Cell.CELL_TYPE_BOOLEAN: // 4
-			logger.trace("(row.col) = (" + cell.getRowIndex() + "," + cell.getColumnIndex() + ")" + "cellValue = " + cell);
+			logger.trace("Cell.CELL_TYPE_BOOLEAN:" + "(row.col) = (" + cell.getRowIndex() + "," + cell.getColumnIndex() + ")" + "cellValue = " + cell);
 			tempCellValue = DOUBLEQUOT + cell.getBooleanCellValue() + DOUBLEQUOT;
 			break;
 		case Cell.CELL_TYPE_ERROR: // 5
-			logger.trace("(row.col) = (" + cell.getRowIndex() + "," + cell.getColumnIndex() + ")" + "cellValue = " + cell);
+			logger.trace("Cell.CELL_TYPE_ERROR:" + "(row.col) = (" + cell.getRowIndex() + "," + cell.getColumnIndex() + ")" + "cellValue = " + cell);
 			tempCellValue = DOUBLEQUOT + cell.getErrorCellValue() + DOUBLEQUOT;
 			break;
 		default:
-			logger.trace("(row.col) = (" + cell.getRowIndex() + "," + cell.getColumnIndex() + ")" + "cellValue = " + cell);
+			logger.trace("default:" + "(row.col) = (" + cell.getRowIndex() + "," + cell.getColumnIndex() + ")" + "cellValue = " + cell);
 			tempCellValue = DOUBLEQUOT + "" + DOUBLEQUOT;
 		}
 		return tempCellValue;
