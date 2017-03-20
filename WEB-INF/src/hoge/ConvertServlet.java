edb97ffef488bbf69f520bf197863ae049694487
your user name <tsuniki.miniki@gmail.com>
2017-03-20 23:46:31 +0900

    part

--- WEB-INF/src/hoge/ConvertServlet.java
+++ WEB-INF/src/hoge/ConvertServlet.java
@@ -15,6 +15,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 import org.apache.log4j.Logger;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
@@ -24,7 +25,8 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
-@WebServlet("/UploadServlet")
+
+
 @MultipartConfig(location="/tmp", maxFileSize=1048576)
 public class ConvertServlet extends HttpServlet {
 	final Logger logger = Logger.getLogger(ConvertServlet.class);
@@ -35,7 +37,22 @@ public class ConvertServlet extends HttpServlet {
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		System.out.println("para = " + request.getParameter("targetFile[]"));
+		for (Part part : request.getParts()) {
+			System.out.println("------------------");
+			System.out.println("parts = " + part);
+			String uploadedFileName = this.getFileName(part);
+			System.out.println("uploadedFileName = " + uploadedFileName);
+			part.write(getServletContext().getRealPath("/WEB-INF/uploaded") + "/" + uploadedFileName);
+		}
+		
+		
+		
+//		Part part = request.getPart("targetFile[]");
+//		System.out.println("part = " + part);
+//		String uploadedFileName = this.getFileName(part);
+//		System.out.println("uploadedFileName = " + uploadedFileName);
+//		System.out.println("real path = " + getServletContext().getRealPath("/WEB-INF/uploaded"));
+//		part.write(getServletContext().getRealPath("/WEB-INF/uploaded") + "/" + uploadedFileName);
 		
 		
 		logger.trace("START CONVERT PROCESS");
@@ -159,4 +176,18 @@ public class ConvertServlet extends HttpServlet {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/Result.jsp");
 		dispatcher.forward(request, response);
 	}
+	
+	private String getFileName(Part part) {
+        String name = null;
+        for (String dispotion : part.getHeader("Content-Disposition").split(";")) {
+            if (dispotion.trim().startsWith("filename")) {
+                name = dispotion.substring(dispotion.indexOf("=") + 1).replace("\"", "").trim();
+                name = name.substring(name.lastIndexOf("\\") + 1);
+                break;
+            }
+        }
+        return name;
+    }
+	
+	
 }
